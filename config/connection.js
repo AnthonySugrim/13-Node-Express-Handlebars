c8f892a7052a6d5d2efb3d79c184ebee876a64f4
stardust191 <57815678+stardust191@users.noreply.github.com>
2020-08-02 14:30:04 -0400

    completed

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,10 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3000,
+    port: 3306,
     user: "root",
-    password: "Rickforeva22"
+    password: "Rickforeva22",
+    database: "burgers_db"
 });
 
 connection.connect(function (err) {
--- config/orm.js
+++ config/orm.js
@@ -1,51 +1,75 @@
-var connection = require("./connection");
-const { connect } = require("./connection");
+var connection = require("./connection.js");
 
-// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. 
-// These are the methods you will need to use in order to retrieve and store data in your database.
+function printQuestionMarks(num) {
+  var arr = [];
 
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
+  return arr.toString();
+}
 
-// * Export the ORM object in `module.exports`.
+function objToSql(ob) {
+  // column1=value, column2=value2,...
+  var arr = [];
 
-var orm ={
- all: function(tableInput, cb){
-     // * `selectAll()`
-     var queryString = "SELECT * FROM " + tableInput + ";";
-     connection.query(queryString, function(err, result){
-         if(err){
-             throw err
-         }
-         cb(result);
-     });
-},
+  for (var key in ob) {
+    arr.push(key + "=" + ob[key]);
+  }
 
-// create: function(table, cols, cb, vals){
-//     // * `insertOne()`
-//     var queryString = "INSERT INTO " + table;
+  return arr.toString();
+}
 
-//     queryString += " (";
-//     queryString += cols.toString();
-//     queryString += " )";
-//     queryString += "VALUES (";
-//     queryString += ") ";
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // vals is an array of values that we want to save to cols
+  // cols are the columns we want to insert the values into
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-//    console.log(queryString);
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-//    connection.query(queryString, vals, function(err, result){
-//        if (err){
-//            throw err
-//        }
-//        cb(result);
-//    })
-// },
+    console.log(queryString);
 
-// // * `updateOne()`
-// update: function(data){
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // objColVals would be the columns and values that you want to update
+  // an example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-// }
-
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
